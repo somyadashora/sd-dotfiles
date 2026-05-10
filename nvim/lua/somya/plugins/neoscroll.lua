91a66368e237231281947eedb2de0555a1f6482c
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2026-05-10 14:26:38 +0000

    modified easing timings neoscroll

--- nvim/lua/somya/plugins/neoscroll.lua
+++ nvim/lua/somya/plugins/neoscroll.lua
@@ -5,10 +5,10 @@ return {
       local neoscroll = require("neoscroll")
       neoscroll.setup({ easing = "quadratic", mappings = {} })
       local keymap = {
-        ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 350, easing = "quadratic" }) end,
-        ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 350, easing = "quadratic" }) end,
-        ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 350, easing = "sine" }) end,
-        ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 350, easing = "sine" }) end,
+        ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 200, easing = "quadratic" }) end,
+        ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 200, easing = "quadratic" }) end,
+        ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 200, easing = "sine" }) end,
+        ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 200, easing = "sine" }) end,
         ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 }) end,
         ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 100 }) end,
         ["zt"] = function() neoscroll.zt({ half_win_duration = 150 }) end,
