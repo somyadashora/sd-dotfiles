003cac79a47f0290c7131f1cd41a69e164928540
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2026-05-10 14:04:18 +0000

    added neoscroll support

---
+++ nvim/lua/somya/plugins/neoscroll.lua
@@ -0,0 +1,20 @@
+return {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      local neoscroll = require("neoscroll")
+      neoscroll.setup({ easing = "quadratic", mappings = {} })
+      local keymap = {
+        ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 250, easing = "sine" }) end,
+        ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 250, easing = "sine" }) end,
+        ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 450, easing = "circular" }) end,
+        ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 450, easing = "circular" }) end,
+        ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 }) end,
+        ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 100 }) end,
+        ["zt"] = function() neoscroll.zt({ half_win_duration = 250 }) end,
+        ["zz"] = function() neoscroll.zz({ half_win_duration = 250 }) end,
+        ["zb"] = function() neoscroll.zb({ half_win_duration = 250 }) end,
+      }
+      for key, func in pairs(keymap) do vim.keymap.set({ "n", "v", "x" }, key, func) end
+    end,
+  }
