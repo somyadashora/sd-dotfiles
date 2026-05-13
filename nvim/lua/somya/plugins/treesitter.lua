1ad52cfc270fae96f78b068b24e4998eac1df05f
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2026-05-13 09:54:10 +0000

    updated treesitter to support termux

--- nvim/lua/somya/plugins/treesitter.lua
+++ nvim/lua/somya/plugins/treesitter.lua
@@ -1,35 +1,17 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.config")
-
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-        -- List languages you want to disable here (e.g., "python", "c")
-        -- disable = { "python" }, 
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
+eturn {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+    },
+    config = function()
+      local parsers = {
         "json",
         "yaml",
         "markdown",
         "markdown_inline",
         "python",
-        "asciidoc",
         "bash",
         "lua",
         "vim",
@@ -43,16 +25,32 @@ return {
         "vhdl",
         "systemverilog",
         "verilog",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
-}
+      }
+
+      local treesitter = require("nvim-treesitter")
+
+      treesitter.setup({
+        install_dir = vim.fn.stdpath("data") .. "/site",
+      })
+
+      if vim.fn.executable("tree-sitter") == 1 then
+        treesitter.install(parsers)
+      else
+        vim.notify(
+          "tree-sitter CLI not found; skipping parser installation",
+          vim.log.levels.WARN
+        )
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local ok = pcall(vim.treesitter.start, args.buf)
+          if ok then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+
+      require("nvim-ts-autotag").setup()
+    end,
+  }
